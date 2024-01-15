3b0261b95573e1d4caa0bc6c90b0176d8b1e6905
Loup <geode_etonnement0q@icloud.com>
2024-01-15 15:32:12 +0100

    Initial Commit

---
+++ iTunesExplorer/iTunesExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesExplorerApp.swift
+//  iTunesExplorer
+//
+//  Created by Loup Martineau on 15/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct iTunesExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
