6e8455344d622fac3fd6c4439321a59c895c7207
Loup Martineau <loumarti@z1r5p1.42lyon.fr>
2024-01-16 12:30:15 +0100

    passing sample code snippet

--- iTunesExplorer/ContentView.swift
+++ iTunesExplorer/ContentView.swift
@@ -22,6 +22,67 @@ struct ContentView: View {
     }
 }
 
+// - MARK: to use textfield with button
+/*
+    start from : https://developer.apple.com/tutorials/app-dev-training/creating-the-edit-view
+
+      HStack {
+
+                    TextField("New Attendee", text: $newAttendeeName)
+
+                    Button(action: {
+
+                        withAnimation {
+
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+
+                            scrum.attendees.append(attendee)
+
+                            newAttendeeName = ""
+
+                        }
+
+                    }) {
+
+                        Image(systemName: "plus.circle.fill")
+
+                    }
+
+                    .disabled(newAttendeeName.isEmpty)
+
+                }
+        
+        ----------------------------
+
+        vue modale :
+
+        @State private var isPresentingEditView = false
+
+        .sheet(isPresented: $isPresentingEditView) {
+
+            DetailEditView()
+
+        }
+
+        et (de HWS)
+
+        struct SheetView: View {
+            @Environment(\.dismiss) var dismiss
+
+            var body: some View {
+                Button("Press to dismiss") {
+                    dismiss()
+                }
+                .font(.title)
+                .padding()
+                .background(.black)
+            }
+        }
+
+        ---------------------------------
+
+*/
+
 //#Preview {
 //    ContentView()
 //}
